b77c54bd0375ea67fc675f54ef3c9719346f0237
Wilson Lucena <wilsonllucena@gmail.com>
2023-07-18 00:58:19 -0300

    Pagina de about criada

---
+++ src/app/about/page.tsx
@@ -0,0 +1,28 @@
+import {Container, Subtitle, Paragraph, Title} from "@/app/about/style";
+
+export default function About() {
+    return (
+        <Container>
+            <Title>Sobre mim</Title>
+
+            <Subtitle>Sejam bem-vindos ao meu blog! </Subtitle>
+            <Paragraph>Aqui, compartilho com vocês minha jornada apaixonada pelo desenvolvimento de aplicações web e pela busca incessante
+                de soluções inovadoras que elevam a qualidade das entregas. Como líder de equipes técnicas, sempre me empenhei em
+                orientar meus times para alcançar os melhores resultados possíveis, demonstrando que a paixão pela solução de
+                Grande parte do meu conhecimento técnico se debruça sobre o uso de tecnologias como
+                problemas e a inovação são motores poderosos para a excelência.
+            </Paragraph>
+            <Paragraph>Ao longo da minha carreira, pude aplicar minha expertise tanto no setor público quanto no privado,
+                trabalhando com softwares nas áreas financeira, educacional e de segurança. Em agências de publicidade, minha atuação se estendeu à criação de websites e aplicativos móveis,
+                sempre guiado pela paixão pelas tecnologias web e pelo desenvolvimento focado na inovação.
+            </Paragraph>
+            <Paragraph>
+                Grande parte do meu conhecimento técnico se debruça sobre o uso de tecnologias como
+                Javascript (React.Js Node.Js), PHP (Laravel e Symfony), além de alguma experiência com Java (Spring Batch).
+            </Paragraph>
+            <Paragraph>Com este blog, espero compartilhar com vocês minhas experiências, aprendizados e insights nesse universo tecnológico fascinante.
+                Sejam todos bem-vindos e sintam-se à vontade para participar com comentários, perguntas e sugestões.
+            </Paragraph>
+        </Container>
+    );
+}
---
+++ src/app/about/style.ts
@@ -0,0 +1,8 @@
+import tw from "tailwind-styled-components";
+
+export  const Container = tw.main``;
+export const Title =    tw.h1`mb-6 text-3xl font-bold`;
+
+export const Subtitle = tw.strong`mb-4 block text-xl font-bold leading-relaxed`;
+
+export const Paragraph =     tw.p`mb-2 text-xl font-normal leading-relaxed text-justify`;
